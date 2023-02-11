b154a2306aa0a5f38a0da83140fc092c2ba82d71
abiodunraheem <raheemabiodun23@gmail.com>
2023-02-12 00:41:06 +0100

    Add popup page to project

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react';
 import './content.css';
 import emailjs from 'emailjs-com';
-import circle1 from '../../assets/circle1.png';
-import circle2 from '../../assets/circle2.png';
+// import circle1 from '../../assets/circle1.png';
+// import circle2 from '../../assets/circle2.png';
 
 const Content = () => {
   const form = useRef();
@@ -15,17 +15,18 @@ const Content = () => {
   };
   return (
     <section id="contact">
-      <h5>Get In Touch</h5>
-      <h2>Contact Me</h2>
-      <div className="circle1">
+      {/* <div className="circle1">
         <img src={circle1} alt="header first circle" />
       </div>
       <div className="circle2">
         <img src={circle2} alt="header second circle" />
-      </div>
-      <div className="container contact_container">
-        <div className="contact_options">
-          <h5>Turn your best customers into Loyal fans</h5>
+      </div> */}
+      <div className="container content-container">
+        <div className="content">
+          <h1>
+            Turn your best customers into
+            <span className="orange"> Loyal fans</span>
+          </h1>
           <p>
             Get ready to revolutionize the way you interact with your customers and
             drive sales with Loyalbaze. Join the waiting list now to be among the first to
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,21 +1,89 @@
-import React from 'react';
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import './header.css';
+// import { GiEternalLove } from 'react-icons/gi';
+// import Contact from '../contact/Contact';
+
+// const Header = () => (
+//   <header>
+//     <div className="container header-container">
+//       <h2>
+//         l
+//         <GiEternalLove class="love" />
+//         yalbaze
+//       </h2>
+//       <button type="submit" className="btn btn-primary">Request a call</button>
+//     </div>
+//   </header>
+// );
+
+// export default Header;
+
+import React, { useRef, useState } from 'react';
+import Modal from 'react-modal';
 import './header.css';
 import { GiEternalLove } from 'react-icons/gi';
+// import './contact.css';
+import emailjs from 'emailjs-com';
 
-const Header = () => (
-  <header>
-    <div className="container header_container">
-      <div>
-        <p>
-          l
-          <GiEternalLove />
-          yalbaze
-        </p>
-        <button type="submit">Request a call</button>
-        <a href="#contact" className="pop-up">Request a call</a>
-      </div>
-    </div>
-  </header>
-);
+const Header = () => {
+  const [modalIsOpen, setmodalIsOpen] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_y1wcamy', 'template_m8qp32i', form.current, '_LZ3I_eYb72pjJF8d');
+    e.target.reset();
+  };
+  return (
+    <section id="contact">
+      <header>
+        <div className="container header-container">
+          <h2>
+            l
+            <GiEternalLove class="love" />
+            yalbaze
+          </h2>
+          <button type="submit" className="btn btn-primary" onClick={() => setmodalIsOpen(true)}>Request a call</button>
+        </div>
+      </header>
+      <Modal
+        isOpen={modalIsOpen}
+        style={
+          {
+            content: {
+              color: '#fff',
+              backgroundColor: '#151729',
+              height: '100vh',
+              overflow: 'hidden',
+            },
+          }
+        }
+      >
+        <div className="container contact-container">
+          <div className="contact_options">
+            <h1>Priority Access</h1>
+            <p>
+              Get ready to revolutionize the way you interact with your customers and
+              drive sales with Loyalbaze. Skip the waitlist and get exclusive priority
+              access to LoyalBaze. Limited slots available. Please tell us a bit about
+              your business and needs, and our consultants will be in touch immediately!
+            </p>
+          </div>
+          <form ref={form} onSubmit={sendEmail}>
+            <h2>Book a consultation with us</h2>
+            <input type="text" name="name" placeholder="Enter your full name" required />
+            <input type="email" name="email" placeholder="Enter your work email" required />
+            <input type="email" name="email" placeholder="Mobile number" required />
+            <input type="email" name="email" placeholder="Enter your work email" required />
+            <textarea name="message" id="" cols="30" rows="10" placeholder="Drop a message" required />
+            <button type="submit" className="btn btn-primary">Send Message</button>
+          </form>
+        </div>
+      </Modal>
+    </section>
+  );
+};
 
 export default Header;
